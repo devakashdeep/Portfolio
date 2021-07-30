f382e0fae324c138ae9327882bd4edfc64353cc5
devakashdeep <akashdeepcse@gmail.com>
2021-07-30 22:29:32 +0530

    'update'

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use('/assets', express.static('assets'))
 
-const DB_URI = `mongodb+srv://n1k1w1k1:24lBhGZwLS5ZwpO8@portfolio.liyth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-mongodb.connect(DB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+mongodb.connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
     app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 })
 
