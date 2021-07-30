ed241580ddcd9dfd33eb958f841046766ba32bbc
devakashdeep <akashdeepcse@gmail.com>
2021-07-30 21:32:16 +0530

    'update'

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const blogRouter = require('./controllers/blogController')
+const homeRouter = require('./routes/homeRouter')
+const mongodb = require('mongoose')
 const app = express()
 
 PORT = process.env.PORT || 8080
@@ -7,9 +10,10 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use('/assets', express.static('assets'))
 
-app.get('/', (req, res) => {
-
-    res.render('home')
+const DB_URI = `mongodb+srv://n1k1w1k1:24lBhGZwLS5ZwpO8@portfolio.liyth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongodb.connect(DB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+    app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
 })
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+app.use(homeRouter)
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,44 @@ openMenu.addEventListener("click", function () {
 closeMenu.addEventListener("click", function () {
     menu.style.display = 'none';
 });
+
+
+const allButton = document.querySelector('#portfolio-all');
+const webButton = document.querySelector('#portfolio-web');
+const mobileButton = document.querySelector('#portfolio-mobile');
+
+const portfolioCard = document.querySelectorAll('.portfolioItems');
+
+allButton.addEventListener("click", function () {
+    allButton.classList.add('btn-primary');
+    webButton.classList.remove('btn-primary');
+    mobileButton.classList.remove('btn-primary');
+    portfolioCard.forEach(item => {
+        item.style.display = 'block';
+    });
+});
+
+webButton.addEventListener("click", function () {
+    webButton.classList.add('btn-primary');
+    allButton.classList.remove('btn-primary');
+    mobileButton.classList.remove('btn-primary');
+    portfolioCard.forEach(item => {
+        if (item.dataset.id != 'Website') {
+            item.style.display = 'none';
+        } else {
+            item.style.display = 'block';
+        }
+    });
+});
+mobileButton.addEventListener("click", function () {
+    mobileButton.classList.add('btn-primary');
+    allButton.classList.remove('btn-primary');
+    webButton.classList.remove('btn-primary');
+    portfolioCard.forEach(item => {
+        if (item.dataset.id != 'Mobile') {
+            item.style.display = 'none';
+        } else {
+            item.style.display = 'block';
+        }
+    });
+});
